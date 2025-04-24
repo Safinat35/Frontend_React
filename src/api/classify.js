c889fc91676020d4e9851b267354748e418852fa
Safinat35 <safinat.alamri@gmail.com>
2025-04-24 15:04:25 +0300

    Fix API request bug

--- src/api/classify.js
+++ src/api/classify.js
@@ -1,7 +1,14 @@
 export const classifyText = async (text) => {
-    const response = await fetch('http://127.0.0.1:8000/api/classify/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+    // const response = await fetch('http://127.0.0.1:8000/api/classify/', {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({ text }),
+    // });
+    const response = await fetch("https://backend-django-mwn4.onrender.com/api/predict/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
         body: JSON.stringify({ text }),
     });
 
